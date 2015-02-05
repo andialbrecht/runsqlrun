4717f815a74d1f8b78aeaaaabb8e0bef00eaf06a
Andi Albrecht <albrecht.andi@gmail.com>
2015-02-05 17:05:43 +0100

    Add support for MySQL databases.

--- rsr/connections/backends/__init__.py
+++ rsr/connections/backends/__init__.py
@@ -5,6 +5,7 @@ DriverSpec = namedtuple('DriverSpec', 'key, name, module')
 
 DRIVERS = [
     DriverSpec('psql', 'PostgreSQL', 'psycopg2'),
+    DriverSpec('mysql', 'MariaDB / MySQL', 'mysql.connector'),
     DriverSpec('oracle', 'Oracle', 'cx_Oracle'),
 ]
 
@@ -13,6 +14,9 @@ def get_backend(config):
     if config.get('driver', None) == 'psql':
         from rsr.connections.backends import psql
         return psql.Driver(config)
+    elif config.get('driver', None) == 'mysql':
+        from rsr.connections.backends import mysql
+        return mysql.Driver(config)
     elif config.get('driver', None) == 'oracle':
         from rsr.connections.backends import oracle
         return oracle.Driver(config)
---
+++ rsr/connections/backends/mysql.py
@@ -0,0 +1,79 @@
+from rsr.connections.backends.base import BaseDriver
+from rsr.schema import dbo
+from rsr.schema.base import BaseSchemaProvider
+
+try:
+    import mysql.connector as mysql_connector
+except ImportError:
+    mysql_connector = None
+
+
+class MySqlSchema(BaseSchemaProvider):
+
+    def refresh(self, schema):
+        tables = {}
+        sql = SQL_TABLES % {'schema': self.driver.config['db']}
+        for table in self.driver.execute_raw(sql):
+            uid = '{}.{}'.format(table[0], table[1])
+            t = dbo.Table(uid, table[1], table[2])
+            schema.add_object(t)
+            tables[uid] = t
+        sql = SQL_COLUMNS % {'schema': self.driver.config['db']}
+        for col in self.driver.execute_raw(sql):
+            table_uid = '{}.{}'.format(col[0], col[1])
+            table = tables[table_uid]
+            col_uid = '{}.{}'.format(table_uid, col[2])
+            col = dbo.Column(col_uid, col[2], description=col[4], order=col[3])
+            table.add_column(col)
+
+
+class Driver(BaseDriver):
+    dbapi = mysql_connector
+    schema_class = MySqlSchema
+
+    def get_connect_params(self):
+        args = ()
+        kwargs = {}
+        kwargs['host'] = self.config.get('host', 'localhost')
+        if 'port' in self.config:
+            kwargs['port'] = self.config['port']
+        kwargs['user'] = self.config.get('username', None)
+        kwargs['passwd'] = self.config.get('password', None)
+        kwargs['database'] = self.config.get('db', None)
+        return args, kwargs
+
+
+SQL_TABLES = """
+SELECT table_schema,
+       table_name,
+       table_comment
+FROM information_schema.tables
+WHERE table_schema = '%(schema)s'
+"""
+
+
+SQL_COLUMNS = """
+SELECT table_schema,
+       TABLE_NAME,
+       COLUMN_NAME,
+       ordinal_position,
+       column_comment
+FROM information_schema.columns
+where table_schema = '%(schema)s'
+"""
+
+
+SQL_CONSTRAINTS = """
+select f.oid,
+       f.conname,
+       f.contype,
+       f.conrelid,
+       f.conkey,
+       f.confrelid,
+       f.confkey
+from pg_catalog.pg_constraint f
+join pg_catalog.pg_class c on c.oid = f.conrelid
+join pg_catalog.pg_namespace n on n.oid = c.relnamespace
+where c.relkind = 'r'
+  and n.nspname = 'public';
+"""
